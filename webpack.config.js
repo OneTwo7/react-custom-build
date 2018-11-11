cba6e3d945420fdbd470a57fd3553ce4fb05d787
onetwo7 <lahoohal@gmail.com>
2018-11-12 00:30:09 +0300

    postcss-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,37 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractCSS = new ExtractTextPlugin('styles.min.css');
 
 const htmlWebpackPlugin = new HtmlWebPackPlugin({
-  template: "./src/index.html",
-  filename: "./index.html"
+    template: './src/index.html',
+    filename: './index.html'
 });
 
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader"
-          }
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/,
+                use: extractCSS.extract([
+                    'css-loader',
+                    'postcss-loader'
+                ])
+            },
+            {
+                test: /\.html$/,
+                use: [{
+                    loader: 'html-loader'
+                }]
+            }
         ]
-      }
-    ]
-  },
-  plugins: [htmlWebpackPlugin]
+    },
+    plugins: [htmlWebpackPlugin, extractCSS]
 };
